e94852ac0b202fc61e3bcfe260f176c0e9a4fd28
Courtney Phillips <Courtney@epicodus.com>
2016-09-09 15:27:32 -0700

    Add mDimes; member property to represent number of dimes. Set to 20 by default in constructor.

--- src/main/java/ChangeMachine.java
+++ src/main/java/ChangeMachine.java
@@ -1,12 +1,18 @@
 public class ChangeMachine {
   private int mQuarters;
+  private int mDimes;
 
   public ChangeMachine() {
     mQuarters = 20;
+    mDimes = 20;
   }
 
   public int getQuarters(){
     return mQuarters;
+  }
+
+  public int getDimes(){
+    return mDimes;
   }
 
 
